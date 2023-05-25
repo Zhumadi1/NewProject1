914d9b905d8c9f5931a43ef120914e927fd9ac25
Zhumadi1 <dzumaabdykadyrov@gmail.com>
2023-05-25 12:45:06 +0300

    w

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import company.model.User;
 import company.model.enam.Gender;
 import company.service.Impl.UserServiceImpl;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
@@ -11,17 +12,19 @@ public class Main {
        User user = new User(1,"Zhumadil",Gender.MALE,24);
        User user1 = new User(2,"Aksana",Gender.FEMALE,24);
        User user2 = new User(3,"Artur",Gender.MALE,14);
+       User[] users = new User[]{user,user1,user2};
+
 
 
       UserServiceImpl userService = new UserServiceImpl();
-      userService.addUser(user);
-      userService.addUser(user1);
-      userService.addUser(user2);
-        System.out.println(userService.getUserById(1));
-        System.out.println(userService.allUser());
-        userService.deleteById(2);
-        System.out.println(userService.allUser());
+        for (User user3 : users) {
+            userService.addUser(user3);
+        }
+            System.out.println(userService.getUserById(1));
+            System.out.println(userService.allUser());
+            userService.deleteById(2);
+            System.out.println(userService.allUser());
 
 
-    }
+        }
 }
